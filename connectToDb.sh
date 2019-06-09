be8755749cee53c4128bce63ea170029b194bcaa
Paula <poseroff@itba.edu.ar>
2019-06-09 16:51:45 -0300

    Specify username added

--- connectToDb.sh
+++ connectToDb.sh
@@ -1,1 +1,16 @@
-ssh bail@pampero.itba.edu.ar -L 5432:bd1.it.itba.edu.ar:5432
+#!/bin/bash
+
+# Declarations
+DB_SERVER='pampero.itba.edu.ar'
+DB_ADDRESS='bd1.it.itba.edu.ar'
+DB_PORT='5432'
+# endDeclarations
+
+if [ $# -lt 1 ]
+then
+	echo "Must provide username for $DB_SERVER"
+	exit 1
+fi
+
+ssh $1@$DB_SERVER -L $DB_PORT:$DB_ADDRESS:$DB_PORT
+ls
--- runImport.sh
+++ runImport.sh
@@ -1,2 +1,9 @@
+#!/bin/bash
 
-psql -h bd1.it.itba.edu.ar -U bail -f runImport.sql PROOF
+if [ $# -lt 1 ]
+then
+	echo "Must provide username"
+	exit 1
+fi
+
+psql -h bd1.it.itba.edu.ar -U $1 -f runImport.sql PROOF
