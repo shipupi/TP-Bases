6bf958feb0d126eefcddb466f614c64d232eabea
Paula <poseroff@itba.edu.ar>
2019-06-09 19:24:38 -0300

    Created temporary fix

--- runImport.sql
+++ runImport.sql
@@ -21,5 +21,25 @@ CREATE TABLE intermedia(
 
 \copy intermedia from SalesbyRegion.csv header delimiter ',' csv;
 
+-- Borrar tabla defiitiva (por si quedo de run anterior)
 
-select * from intermedia;
+DROP TABLE IF EXISTS intermedia;
+
+-- Crear tabla definitiva
+
+CREATE TABLE definitiva(
+        Sales_Date DATE NOT NULL,
+        Product_Type TEXT NOT NULL,
+        Territory TEXT NOT NULL,
+        Sales_Channel TEXT NOT NULL,
+        Customer_Type TEXT NOT NULL,
+        Revenue FLOAT,
+        Cost FLOAT
+);
+
+INSERT INTO definitiva
+SELECT TO_DATE(month, 'yy-Mon') as sales_date, product_type, territory, sales_channel, customer_type, SUM(revenue) as revenue, cost
+FROM intermedia
+GROUP BY month, product_type, territory, sales_channel, customer_type, cost;
+
+-- Seguir aca abajo
